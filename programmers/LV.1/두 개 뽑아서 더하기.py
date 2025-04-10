6393a094b9970c7c8539f325a277381b3cbd8173
donghwan kim <dhkim000404@naver.com>
2025-04-10 11:59:08 +0900

    commit

---
+++ programmers/LV.1/두 개 뽑아서 더하기.py
@@ -0,0 +1,14 @@
+"""
+정수 배열 numbers가 주어집니다. 
+numbers에서 서로 다른 인덱스에 있는 두 개의 수를 뽑아 더해서 만들 수 있는 모든 수를 
+배열에 오름차순으로 담아 return 하도록 solution 함수를 완성해주세요.
+"""
+
+def solution(numbers):
+    answer = []
+    for i in range(len(numbers)):
+        for j in range(i+1,len(numbers)):
+            key = numbers[i] + numbers[j]
+            if key not in answer:
+                answer.append(key)
+    return sorted(answer)
