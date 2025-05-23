082b21e1074d65d5d39f464c17945444b5a302c4
donghwan kim <dhkim000404@naver.com>
2025-05-23 11:06:40 +0900

    commit

---
+++ baekjoon/Bronze/알파벳 찾기.py
@@ -0,0 +1,20 @@
+"""
+알파벳 소문자로만 이루어진 단어 S가 주어진다. 
+각각의 알파벳에 대해서, 단어에 포함되어 있는 경우에는 처음 등장하는 위치를, 
+포함되어 있지 않은 경우에는 -1을 출력하는 프로그램을 작성하시오.
+
+첫째 줄에 단어 S가 주어진다. 단어의 길이는 100을 넘지 않으며, 알파벳 소문자로만 이루어져 있다.
+"""
+
+import sys
+input = sys.stdin.readline
+
+S = input()
+ary = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+
+for i in range(len(ary)):
+    if ary[i] in S:
+        x = S.index(ary[i])
+        print(x, end = ' ')
+    else:
+        print(-1, end = ' ')
