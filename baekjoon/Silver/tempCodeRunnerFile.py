41cf3a8dcf217506d23b7bf6c970f65a14db17ef
donghwan kim <dhkim000404@naver.com>
2025-06-14 11:53:46 +0900

    commit

--- baekjoon/Silver/tempCodeRunnerFile.py
+++ baekjoon/Silver/tempCodeRunnerFile.py
@@ -1,33 +1,17 @@
-# 내가 푼 것 -> 시간초과
-# while (1):
-#     n = int(input())
-#     if n == 0:
-#         break
-#     result = None
+dp[1][1] = 1
+dp[2][2] = 1
+dp[3][1] = 1
+dp[3][2] = 1
+dp[3][3] = 1
 
-#     # 2부터 n의 절반까지 반복하며 소수 찾기
-#     for i in range(2, n//2 + 1):
-#         tmp = True
+# 점화식
+for n in range(4, 100001):
+    dp[n][1] = (dp[n-1][2] + dp[n-1][3])
+    dp[n][2] = (dp[n-2][1] + dp[n-2][3])
+    dp[n][3] = (dp[n-3][1] + dp[n-3][2])
 
-#         # i가 소수인지 확인
-#         for j in range(2, int(i ** 0.5) + 1):
-#             if i % j == 0:
-#                 tmp = False
-#                 break
+for _ in range(T):
+    n = int(input())
+    answer = (dp[n][1] + dp[n][2] + dp[n][3]) % 1000000009
 
-#         if tmp:
-#             # n-i가 소수인지
-#             for k in range(2, int((n-i)**0.5) + 1):
-#                 if (n-i) % k == 0:
-#                     tmp = False
-#                     break
-
-#             if tmp:
-#                 # 조건을 만족하는 소수쌍 저장
-#                 result = (i, n-i) # 가장 마지막에 발견된 소수 쌍이 b-a가 가장 큼
-
-#     if result:
-#         a, b = result
-#         print(n, '=', a, '+', b)
-#     else:
-#         print("Goldbach's conjecture is wrong.")
+    print(answer)
